221fe61a9389c97c4e6e43b476ae2c30580bc97b
Florian Keller <flori.keller@online.de>
2021-04-19 16:03:37 +0200

    citation algo: now supports multiple refs to same url

--- assets/util.js
+++ assets/util.js
@@ -16,22 +16,29 @@ import * as nanoid from 'nanoid';
  * @returns {string}
  */
 export function enhanceCitations(contentHtml) {
-    // contentHtml = "<p>Lalala \\citep{https://example.com} \\citep{https://google.com} lalala.</p>"; // XXX
+    // contentHtml = "<p>Lalala \\citep{https://example.com} \\citep{https://google.com} lalala.</p>";
     let citationCounter = 1;
-    let citations = [];
+    let citations = [/*
+        { citationId: string, citationUrl: string, citationCounter: number(1-based-index) },
+        ...
+    */];
     contentHtml = contentHtml.replace(/\\citep{([^}]+)}/g, (match, citationUrl) => {
-        let citationId = `citation-${ nanoid.customAlphabet('abcdefghijklmnopqrstuvwxyz0123456789', 10)() }`;
-        // let citationId = `citation-${citationCounter}`; // XXX
-        citations.push({ citationId, citationUrl });
-        return `<a href="#${citationId}">[${citationCounter++}]</a>`;
+        const existingCitation = citations.filter(c => c.citationUrl === citationUrl)[0];  // `undefined` most of the times
+        if (existingCitation) {
+            return `<a href="#${existingCitation.citationId}">[${existingCitation.citationCounter}]</a>`;
+        } else {
+            // we have to make a new citation object
+            let citationId = `citation-${ nanoid.customAlphabet('abcdefghijklmnopqrstuvwxyz0123456789', 10)() }`;
+            citations.push({ citationId, citationUrl, citationCounter });
+            return `<a href="#${citationId}">[${citationCounter++}]</a>`;
+        }
     });
     contentHtml += `
         <table>
             <tbody>
                 ${(() => {
                     let tableHtml = "";
-                    citations.forEach(({ citationId, citationUrl }, i) => {
-                        const citationCounter = i + 1;
+                    citations.forEach(({ citationId, citationUrl, citationCounter }) => {
                         tableHtml += `<tr id="${citationId}">
                             <td>[${citationCounter}]</td>
                             <td><a href="${citationUrl}" target="_blank">${citationUrl}</a></td>
